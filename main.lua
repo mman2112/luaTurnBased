d37f0eabd4accf3f8916b3f21ab91ce7865aa4be
mman2112 <mman2112@gmail.com>
2016-03-02 16:52:54 -0500

    circle bro

--- main.lua
+++ main.lua
@@ -1,25 +1,15 @@
-boxVel = 0
-boxPos = 0
+object = {
+	x=400,
+	y=300,
+	r=8
+}
+function love.load()
+
+end
 function love.update(dt)
-	boxPos = boxPos + boxVel * dt
-	boxVel = boxVel - 1600 * dt
-	if boxPos < 0 then	--so box can't fall through floor
-		boxPos = 0
-		boxVel = 0
-	end
-end
-function jump()
-	boxVel = math.max(0, boxVel)
-	boxVel = boxVel + 800
-end
-function love.keypressed(key)
-	if key == " " then
-		jump()
-	end
+
 end
 function love.draw()
 	love.graphics.setColor(255, 255, 255)
-	love.graphics.rectangle("fill", 0, 0, 800, 600)	--draw white background
-	love.graphics.setColor(200, 0, 255)
-	love.graphics.rectangle("fill", 350, 520-boxPos, 80, 80)	--draw fred
+	love.graphics.circle("fill", object.x, object.y, object.r, 16)
 end
