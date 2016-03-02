f18974e7606c048a6ea8dfa06fd7d7cdc916f19e
mman2112 <mman2112@gmail.com>
2016-03-02 18:21:44 -0500

    SNOW (for real this time)

--- main.lua
+++ main.lua
@@ -1,16 +1,17 @@
-object = {
-	x=400,
-	y=300,
-	r=8
-}
 function love.load()
-
+	snowflake = love.graphics.newImage("snow.png")
+	snow = love.graphics.newParticleSystem(snowflake, 256)
+	snow:setParticleLifetime(10)
+	snow:setEmissionRate(25)
+	snow:setDirection(math.pi / 2)
+	snow:setSpeed(100)
+	snow:setAreaSpread("uniform", 400, 0)
+	snow:setSizes(0.5)
+	snow:setLinearAcceleration(-10, -5, 10, 15)
 end
 function love.update(dt)
-
+	snow:update(dt)
 end
 function love.draw()
-	love.graphics.setColor(255, 255, 255)
-	love.graphics.circle("fill", object.x, object.y, object.r, 16)
+	love.graphics.draw(snow, 400, -32)
 end
-#BABABA
