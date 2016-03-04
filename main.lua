d55da68b58371a1e5240d46d6de1392bb2e913ee
mman2112 <mman2112@gmail.com>
2016-03-03 19:22:53 -0500

    ayy lmao

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function love.load()
 	snowflake = love.graphics.newImage("snow.png")
 	snow = love.graphics.newParticleSystem(snowflake, 256)
@@ -14,4 +15,30 @@ function love.update(dt)
 end
 function love.draw()
 	love.graphics.draw(snow, 400, -32)
+=======
+boxVel = 0
+boxPos = 0
+function love.update(dt)
+	boxPos = boxPos + boxVel * dt
+	boxVel = boxVel - 1600 * dt
+	if boxPos < 0 then	--so box can't fall through floor
+		boxPos = 0
+		boxVel = 0
+	end
+end
+function jump()
+	boxVel = math.max(0, boxVel)
+	boxVel = boxVel + 800
+end
+function love.keypressed(key)
+	if key == " " then
+		jump()
+	end
+end
+function love.draw()
+	love.graphics.setColor(255, 255, 255)
+	love.graphics.rectangle("fill", 0, 0, 800, 600)	--draw white background
+	love.graphics.setColor(200, 0, 255)
+	love.graphics.rectangle("fill", 350, 520-boxPos, 80, 80)	--draw fred
+>>>>>>> parent of d37f0ea... circle bro
 end
